b355ccb9701935e7f903c23371a16a117eafdb67
hyejun <hyejunkkim228@gmail.com>
2026-06-26 15:35:53 +0900

    feat: header 컴포넌트 종류 추가

--- components/header/header.js
+++ components/header/header.js
@@ -11,6 +11,20 @@ export function header({ type = "default" } = {}) {
     `;
   }
 
+  if (type === "withProfile") {
+    return `
+      <header class="header">
+        <div class="header__inner header__inner--with-profile">
+          <div class="header__left-space"></div>
+          <h1 class="header__title">아무 말 대잔치</h1>
+          <button class="header__profile-button" type="button" aria-label="프로필 메뉴">
+            <div class="header__profile-image"></div>
+          </button>
+        </div>
+      </header>
+    `;
+  }
+
   return `
     <header class="header">
       <div class="header__inner">
