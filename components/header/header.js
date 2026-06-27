b4a8bb8cbb27f7f68b4eac8c8391d2c27888f78e
hyejun <hyejunkkim228@gmail.com>
2026-06-27 16:31:52 +0900

    feat: header에 프로필 추가

--- components/header/header.js
+++ components/header/header.js
@@ -17,9 +17,41 @@ export function header({ type = "default" } = {}) {
         <div class="header__inner header__inner--with-profile">
           <div class="header__left-space"></div>
           <h1 class="header__title">아무 말 대잔치</h1>
-          <button class="header__profile-button" type="button" aria-label="프로필 메뉴">
-            <div class="header__profile-image"></div>
-          </button>
+
+          <div class="header__profile-wrapper">
+            <button class="header__profile-button" type="button" aria-label="프로필 메뉴">
+              <div class="header__profile-image"></div>
+            </button>
+
+            <div class="header__dropdown">
+              <button class="header__dropdown-item header__menu-profile-edit" type="button">회원정보수정</button>
+              <button class="header__dropdown-item header__menu-password-edit" type="button">비밀번호수정</button>
+              <button class="header__dropdown-item header__menu-logout" type="button">로그아웃</button>
+            </div>
+          </div>
+        </div>
+      </header>
+    `;
+  }
+
+  if (type === "withBackAndProfile") {
+    return `
+      <header class="header">
+        <div class="header__inner header__inner--with-back-profile">
+          <button class="header__back-button" type="button" aria-label="뒤로가기">&lt;</button>
+          <h1 class="header__title">아무 말 대잔치</h1>
+
+          <div class="header__profile-wrapper">
+            <button class="header__profile-button" type="button" aria-label="프로필 메뉴">
+              <div class="header__profile-image"></div>
+            </button>
+
+            <div class="header__dropdown">
+              <button class="header__dropdown-item header__menu-profile-edit" type="button">회원정보수정</button>
+              <button class="header__dropdown-item header__menu-password-edit" type="button">비밀번호수정</button>
+              <button class="header__dropdown-item header__menu-logout" type="button">로그아웃</button> 
+            </div>
+          </div>
         </div>
       </header>
     `;
