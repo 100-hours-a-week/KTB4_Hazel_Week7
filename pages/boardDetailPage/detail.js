e17550c95255144f6c2965ff0e7cb8b6389ec687
hyejun <hyejunkkim228@gmail.com>
2026-07-09 15:33:11 +0900

    feat: 폰트 추가 및 전체적인 폰트 적용

--- pages/boardDetailPage/detail.js
+++ pages/boardDetailPage/detail.js
@@ -65,7 +65,7 @@ const postDetail = document.querySelector("#postDetail");
 
 function createImageList(images = []) {
   if (!images.length) {
-    return `<div class="detail__image"></div>`;
+    return "";
   }
 
   return `
